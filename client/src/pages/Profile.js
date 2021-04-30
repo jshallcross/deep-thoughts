ac88d4e78abab46357f988b48e9e32a93bc68e7f
jshallcross <jshallcross1@icloud.com>
2021-04-30 16:00:18 -0400

    add friend/thought

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,14 +4,17 @@ import { Redirect, useParams } from 'react-router-dom';
 
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
 
+import { ADD_FRIEND } from '../utils/mutations';
 
-
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 
 const Profile = () => {
+  const [addFriend] = useMutation(ADD_FRIEND);
   const { username: userParam } = useParams();
+
 
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME , {
     variables: { username: userParam }
@@ -36,12 +39,28 @@ if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
     );
   }
 
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s ` : 'your'} profile.
         </h2>
+        { userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
+
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -56,6 +75,7 @@ if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
